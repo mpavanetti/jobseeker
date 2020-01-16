2a6b9ff4bf9512ec835aa9c29807b48aa029b52a
Matheus Pavanetti <matheuspavanetti@gmail.com>
2020-01-16 19:50:05 -0300

    Update JobsTable.php
    check file type to fill the path

--- application/controllers/JobsTable.php
+++ application/controllers/JobsTable.php
@@ -137,6 +137,11 @@ class JobsTable extends BaseController
                 $component_type = "None";
             }
 
+              if ($file == 1){
+                $newComponent_Type = $component_type;
+              }  else {
+                $newComponent_Type = NULL;
+              }
 
 
             $validateComponent = $this->model->validateComponent($job_name, $job_component, $file_path);
