b7ec8c466c4d28d342a31de731834a963ea34889
Matheus Pavanetti <matheuspavanetti@gmail.com>
2020-04-21 16:42:32 -0300

    Job Execution - Added Stop Request
    Has been added stop request to server, in order to stop the running jobs before it get finished.

--- application/views/jobExecution.php
+++ application/views/jobExecution.php
@@ -48,18 +48,18 @@
                           <a class="btn btn-app" id="play">
                             <i class="fa fa-play"></i> Play
                           </a>
-                          <a class="btn btn-app">
-                            <i class="fa fa-pause"></i> Pause
+                          <a class="btn btn-app" id="clean" style="display: none;">
+                            <i class="fa fa-times"></i> Clean
                           </a>
-                          <a class="btn btn-app" id="stop">
-                            <i class="fa fa-stop"></i> Stop
-                          </a>
-                          <a class="btn btn-app">
+                          <a class="btn btn-app" id="repeat" style="display: none;">
                             <i class="fa fa-repeat"></i> Repeat
                           </a>
+                      <span id="spanStop">
+                        
+                      </span>
                     </div>
-                    <div class="overlay" style="display:none;">
-                      <i class="fa fa-refresh fa-spin"></i>
+                      <div id="overlay2" class="overlay" style="display:none;">
+                        <i class="fa fa-refresh fa-spin"></i>
                     </div>
                 </div>
                </div>
@@ -86,11 +86,12 @@
 
     </section>
 </div>
-
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/bower_components/moment/moment.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function(){
 
         var delay = 15000;
+        var errorDelay = 10000;
 
         // get Jenkins credentials
         var jenkins_url = '<?php echo $jenkins_url; ?>';
@@ -105,6 +106,7 @@
           beforeSend: function() {
            
             $('.overlay').show();
+            $('#overlay2').show();
         }
         }).done(function(data) {
 
@@ -129,6 +131,9 @@
          $('.info').remove();
         $('.console').remove();
         $('#selector').val(0);
+        $('#repeat').hide();
+        $('#clean').hide();
+        $('.stop').remove();
 
         if(job == '0'){
             toastr.error("Please, Select an avaiable job to execute.", "Error");
@@ -142,9 +147,51 @@
            
             $('.overlay').show();
             $('#gif').fadeIn();
+            $('#play').hide();
+            
         }
         }).done(function(data) {
+          $('#spanStop').append('<a class="btn btn-app stop" id="stop"><i class="fa fa-stop"></i> Stop</a>');
             toastr.warning("Your Execution Request has been sent to server.", "Request Sent")
+
+
+         var timer = setTimeout(function() {    
+          $.ajax({
+              url: jenkins_url + 'job/'+ job +'/lastBuild/api/json?pretty=true',
+              method: 'GET',
+              headers: {'Authorization': 'Basic ' + btoa(jenkins_username + ':' + jenkins_token)},
+              beforeSend: function() {
+
+              
+               
+            }
+            }).done(function(data) {
+
+              if(data.building == true) {
+
+                toastr.info("Data is still executing, able to stop if needed.")
+                $('#overlay2').hide();
+
+               $('#stop').click(function(){
+                  $.ajax({
+                    url: jenkins_url + 'job/'+ job + '/' + data.id + '/stop',
+                    method: 'POST',
+                    async: false,
+                    headers: {'Authorization': 'Basic ' + btoa(jenkins_username + ':' + jenkins_token)},
+                    beforeSend: function() {
+                      toastr.warning("Your Stop Request has been sent to server.", "Request Sent")
+                  }
+                  }).done(function(data) {
+                      toastr.error("Your Stop Request has been sent to server.", "Operation Aborted")
+                  });
+              }); 
+                
+               
+            }
+          });
+        }, errorDelay);
+
+         $(this).data('timer', timer);
 
         }).fail(function() {
           console.error(arguments);
@@ -158,13 +205,15 @@
               method: 'GET',
               headers: {'Authorization': 'Basic ' + btoa(jenkins_username + ':' + jenkins_token)},
               beforeSend: function() {
+
+              
                
             }
             }).done(function(data) {
 
               if(data.building == false) {
                
-                   $('#info').append($('<div class="col-xs-12 info animated fadeInLeft"><div id="boxHeader" class="box box-success"><div class="box-header with-border"><div class="box-tools pull-right"><button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i> </button></div><h3 class="box-title"><b>Build Execution Info</b></h3></div><div class="box-body"><table class="table table-striped"><tbody><tr><th style="width: 140px">Summary</th><th>Info</th><th>Progress</th><th style="width: 40px">Label</th></tr><tr><td>Status:</td><td><b><span id="status"> </span></b></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Building:</td><td><span id="building"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Build Id:</td><td><span id="buildId"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Queue Id:</td><td><span id="queueId"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></tr><tr><td>URL:</td><td><span id="url"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Description:</td><td><span id="description"></span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Display Name:</td><td><span id="name"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Duration:</td><td><span id="duration"> </span> Mili Seconds</td><td><div class="progress progress-xs progress-striped active"> <div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr></tbody></table></div></div></div>'))
+                   $('#info').append($('<div class="col-xs-12 info animated fadeInLeft"><div id="boxHeader" class="box box-success"><div class="box-header with-border"><div class="box-tools pull-right"><button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i> </button></div><h3 class="box-title"><b>Build Execution Info</b></h3></div><div class="box-body"><table class="table table-striped"><tbody><tr><th style="width: 140px">Summary</th><th>Info</th><th>Progress</th><th style="width: 40px">Label</th></tr><tr><td>Status:</td><td><b><span id="status"> </span></b></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Building:</td><td><span id="building"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Build Id:</td><td><span id="buildId"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Queue Id:</td><td><span id="queueId"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></tr><tr><td>URL:</td><td><span id="url"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Description:</td><td><span id="description"></span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Display Name:</td><td><span id="name"> </span></td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr><tr><td>Elapsed Time:</td><td><span id="duration"> </span></td><td><div class="progress progress-xs progress-striped active"> <div class="progress-bar progress-bar-success" style="width: 100%"></div></div></td><td><span class="badge bg-green">100%</span></td></tr></tbody></table></div></div></div>'))
              
 
                $('#status').html(data.result);
@@ -184,8 +233,8 @@
                
 
                $('#name').html(data.fullDisplayName);
-               $('#duration').html(data.duration);
-
+               $('#duration').html(moment.utc(data.duration).format('HH [Hours, ] mm [Minutes, ] ss [Seconds, ] SSS [Miliseconds.]'));
+            
 
                if(data.result != 'SUCCESS'){
 
@@ -198,6 +247,11 @@
                 toastr.info('Build Id: ' + data.id + '<br> Build Name: ' + data.fullDisplayName ,'Info')
                 toastr.success('The request Build has been successfully executed, please check the logs', 'Executed')
                }
+
+               $('#play').show();
+               $('#clean').show();
+               $('#stop').remove();
+               $('#repeat').show();
 
                $.ajax({
               url: jenkins_url + 'job/'+ job +'/lastBuild/consoleText',
@@ -240,13 +294,16 @@
             }
         })
 
-    $('#stop').click(function(){
+    $('#clean').click(function(){
        var job = $('#selector').val();
 
-        toastr.error('A Stop Request has been sent to server.','Stop Request')
+        toastr.info('Your desktop has been cleaned','Clean Request')
         $('.console').remove();
         $('.info').remove();
         $('#selector').val(0);
+        $('#stop').remove();
+        $('#repeat').hide();
+        $('#clean').hide();
     })
 
         });
