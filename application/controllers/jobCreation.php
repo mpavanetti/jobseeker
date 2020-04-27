f8f2d5aa201866cd2667f03bec8331f7dd06bf89
Matheus Pavanetti <matheuspavanetti@gmail.com>
2020-04-27 18:44:35 -0300

    Job Creation functions
    Job Creation Controller and View,
    - Has been added XML function to append nodes
    - Function to receive abort timeout patterns
    - Fuction to append on xml timestamp,
    - More functions

--- application/controllers/jobCreation.php
+++ application/controllers/jobCreation.php
@@ -28,11 +28,187 @@ class jobCreation extends BaseController
         $this->loadViews("jobCreation", $this->global, NULL, NULL);
     }
 
+    public function send() {
 
- 
+        if($this->isManager() == TRUE)
+        {
+            $this->loadThis();
+        }
+        else
+        {
 
-   
-    
+            $this->load->library('form_validation');
+
+            // Basic inputs
+            $this->form_validation->set_rules('job_name','Job Name','trim|required|max_length[50]');
+            $this->form_validation->set_rules('description','Database Type','trim|required|max_length[5000]');
+
+            // Confirmation Flag
+            $this->form_validation->set_rules('confirmation','Confirmation','trim|required|max_length[1]');
+
+            // Timestamp Option
+            $this->form_validation->set_rules('timestamp','Timestamp','trim|max_length[1]');
+
+            //Abort Build
+            $this->form_validation->set_rules('abort','Abort','trim|max_length[1]');
+            $this->form_validation->set_rules('timeoutStrategy','Time Out Stragegy','trim|max_length[50]');
+            $this->form_validation->set_rules('timeoutMinutes','Time Out in Minutes','trim|max_length[50]');
+            $this->form_validation->set_rules('timeoutSeconds','Time Out in Seconds','trim|max_length[50]');
+
+
+            if($this->form_validation->run() == FALSE)
+            {
+                $this->index();
+            }
+            else
+            {
+                // Basic Inputs
+                $job_name = $this->security->xss_clean($this->input->post('job_name'));
+                $description = $this->security->xss_clean($this->input->post('description')); 
+
+                // Confirmation Checkbox
+                $confirmation = $this->security->xss_clean($this->input->post('confirmation'));
+
+                // Timestamp Checkbox
+                $timestamp = $this->security->xss_clean($this->input->post('timestamp'));
+
+                // Abort the build Checkbox
+                $abort = $this->security->xss_clean($this->input->post('abort'));
+                $timeoutStrategy = $this->security->xss_clean($this->input->post('timeoutStrategy'));
+                $timeoutMinutes = $this->security->xss_clean($this->input->post('timeoutMinutes'));
+                $timeoutSeconds = $this->security->xss_clean($this->input->post('timeoutSeconds'));
+                
+                $Info = array(
+                    'job_name'=>$job_name, 
+                    'description'=>$description, 
+                    'confirmation' => $confirmation,
+                    'timestamp' => $timestamp,
+                    'abort' => $abort,
+                    'timeoutStrategy' => $timeoutStrategy,
+                    'timeoutMinutes' => $timeoutMinutes,
+                    'timeoutSeconds' => $timeoutMinutes,
+                    'creation_date'=> date('Y-m-d H:i:s'),
+                    'owner'=> $this->name
+                );
+
+
+                print_r($Info);
+
+
+                echo '<br><br><hr><br>';
+
+               
+
+                $dom = new DOMDocument();
+
+                $dom->encoding = 'UTF-8';
+
+                $dom->xmlVersion = '1.1';
+
+                $dom->formatOutput = true;
+
+                $xml_file_name = 'config.xml';
+
+                $root = $dom->createElement('project');
+
+              //  $movie_node = $dom->createElement('movie');
+
+               // $attr_movie_id = new DOMAttr('movie_id', '5467');
+
+              //  $movie_node->setAttributeNode($attr_movie_id);
+
+                $node_description = $dom->createElement('description', $description);
+
+                $root->appendChild($node_description);
+
+
+                // Create Trigger Elements
+                $triggers = $dom->createElement('triggers');
+                $hudson_triggers = $dom->createElement('hudson.triggers.TimerTrigger');
+                $spec = $dom->createElement('spec', 'H/15 * * * *');
+                $hudson_triggers->appendChild($spec);  
+                $triggers->appendChild($hudson_triggers);    
+                $root->appendChild($triggers);
+
+
+                // Create buildWrappers Elements
+                $buildWrappers = $dom->createElement('buildWrappers');
+
+                // If option to add timestamp is enabled then
+                if($timestamp == 1) {
+                $hudson_plugins_timestamper = $dom->createElement('hudson.plugins.timestamper.TimestamperBuildWrapper');
+                $attr_hudson_timestamper = new DOMAttr('plugin', 'timestamper@1.10');
+                $hudson_plugins_timestamper->setAttributeNode($attr_hudson_timestamper);
+                $buildWrappers->appendChild($hudson_plugins_timestamper);
+                }
+
+
+                // Abort Build if Stucks Option if enabled then
+                if ($abort == 1){
+                 $hudson_plugins_timeout = $dom->createElement('hudson.plugins.build__timeout.BuildTimeoutWrapper');
+                 $attr_hudson_plugins_timeout = new DOMAttr('plugin', 'build-timeout@1.19');
+                 $hudson_plugins_timeout->setAttributeNode($attr_hudson_plugins_timeout);
+
+                 if ($timeoutStrategy == 'absolute') { // if absolute then
+
+                 $strategy = $dom->createElement('strategy');
+                 $attr_stategy = new DOMAttr('class', 'hudson.plugins.build_timeout.impl.AbsoluteTimeOutStrategy');
+                 $strategy->setAttributeNode($attr_stategy);
+
+                 $timeoutMinutes_node = $dom->createElement('timeoutMinutes', $timeoutMinutes);
+                 $strategy->appendChild($timeoutMinutes_node);
+                 $hudson_plugins_timeout->appendChild($strategy);
+                } else { // if not absolute then
+
+                 $strategy = $dom->createElement('strategy');
+                 $attr_stategy = new DOMAttr('class', 'hudson.plugins.build_timeout.impl.NoActivityTimeOutStrategy');
+                 $strategy->setAttributeNode($attr_stategy);
+                 $timeoutSeconds_node = $dom->createElement('timeoutSecondsString', $timeoutSeconds);
+                 $strategy->appendChild($timeoutSeconds_node);
+                 $hudson_plugins_timeout->appendChild($strategy);
+
+                }
+
+                 $operationList = $dom->createElement('operationList');
+                 $hudson_plugins_abort = $dom->createElement('hudson.plugins.build__timeout.operations.AbortOperation');
+                 $operationList->appendChild($hudson_plugins_abort);
+                 $hudson_plugins_timeout->appendChild($operationList);
+                 $buildWrappers->appendChild($hudson_plugins_timeout);
+                }
+                // End Abort Build if Stucks Option
+
+
+
+                $root->appendChild($buildWrappers);
+
+                // Append document to root node
+                $dom->appendChild($root);
+                // Save XML file
+                $dom->save('xml/'.$xml_file_name);
+                // Read XML File to obtain xml text
+                $content = htmlentities(file_get_contents('xml/'.$xml_file_name));
+
+                // Create flash data to send to view
+                 $this->session->set_flashdata('xml', $content);
+                 $this->session->set_flashdata('job_name', $job_name);
+                 $this->session->set_flashdata('success', 'Your XML File has been successfully created !');
+
+
+                redirect('jobCreation');
+
+            }
+
+        }
+
+
+    }
+
+    public function readXML() {
+
+        header("Content-Type: text/xml");
+        $content = file_get_contents("xml/config.xml");
+        echo $content;
+
+    }
+
 }
-
-?>
--- application/views/jobCreation.php
+++ application/views/jobCreation.php
@@ -20,7 +20,78 @@
     </div>
   </div>
 
-  <div class="row">
+  <div class="row" style="margin-top: 10px; margin-bottom: 40px;">
+   <div class="col-md-12">
+    <?php 
+    $this->load->helper('form');
+    $error = $this->session->flashdata('error');
+    if($error)
+    {
+      ?>
+      <div class="alert alert-danger alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <?php echo $this->session->flashdata('error'); ?>                    
+      </div>
+    <?php } ?>
+    <?php  
+    $success = $this->session->flashdata('success');
+    if($success)
+    {
+      ?>
+      <div class="alert alert-success alert-dismissable destroy">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <?php echo $this->session->flashdata('success'); ?>
+      </div>
+    <?php } ?>
+
+    <div class="row">
+      <div class="col-md-12">
+        <?php echo validation_errors('<div class="alert alert-danger alert-dismissable">', ' <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>'); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="row">
+  <div class="col-lg-6 col-md-6 col-xs-12">
+    <div class="box box-primary">
+      <div class="overlay" style="display:none;">
+        <i class="fa fa-refresh fa-spin"></i>
+      </div>
+      <div class="box-header with-border">
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+        </div>
+        <h3 class="box-title"><b>Job Input Fields</b></h3> 
+      </div>
+
+      <!-- /.box-header -->
+      <!-- form start -->
+      <?php $this->load->helper("form"); ?>
+      <form role="form" id="InsertDbSettings" action="<?php echo base_url() ?>jobCreation/send" method="post" role="form">
+        <div class="box-body">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Job Name</label>
+            <input type="text" name ="job_name" class="form-control" id="job_name" placeholder="Enter Job name">
+          </div>
+          <div class="form-group">
+            <div class="form-group">
+              <label for="description">Description</label>
+              <textarea class="form-control" id="description" value="" name="description" maxlength="500" rows="5" required></textarea>
+            </div>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="confirmation" id="confirmation" value="1"> I <b>Confirm</b> this job build is my responsability and has my confidence.
+            </label>
+          </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+    </div>
+
     <div class="col-lg-6 col-md-6 col-xs-12">
       <div class="box box-primary">
         <div class="overlay" style="display:none;">
@@ -31,110 +102,149 @@
             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
             </button>
           </div>
-          <h3 class="box-title"><b>Job Input Fields</b></h3> 
+          <h3 class="box-title"><b>Options Control Panel</b></h3> 
         </div>
         <!-- /.box-header -->
         <!-- form start -->
-        <form role="form" id="input-form">
-          <div class="box-body">
+        <div class="box-body">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> Trigger Job
+            </label>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> Build Periodicly
+            </label>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="abort" id="abort" value="1"> Abort the job if it's stuck
+            </label>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="timestamp" id="timestamp" value="1"> Add timestamps to the Console Output
+            </label>
+          </div>
+          <div class="form-group">
             <div class="form-group">
-              <label for="exampleInputEmail1">Job Name</label>
-              <input type="text" name ="job_name" class="form-control" id="job_name" placeholder="Enter Job name">
-            </div>
-            <div class="form-group">
-              <div class="form-group">
-                <label for="description">Description</label>
-                <textarea class="form-control" id="description" value="" name="description" maxlength="500" rows="5" required></textarea>
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Add Job Function
+                  <span class="caret"></span>
+                  <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Execute a Windows Command</a></li>
+                  <li><a href="#">Execute a Linux Command</a></li>
+                </ul>
               </div>
             </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" id="confirmation"> I <b>Confirm</b> this job build is my responsability and has my confidence.
-              </label>
+          </div>
+          <div class="form-group">
+            <div class="form-group">
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Actions After Job Execution
+                  <span class="caret"></span>
+                  <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Execute another job</a></li>
+                  <li><a href="#">Email Notification</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Editable Email Notification</a></li>
+                </ul>
+              </div>
             </div>
           </div>
-          <!-- /.box-body -->
-        </div>
-      </div>
-
-      <div class="col-lg-6 col-md-6 col-xs-12">
-        <div class="box box-primary">
-          <div class="overlay" style="display:none;">
-            <i class="fa fa-refresh fa-spin"></i>
+          <div class="form-group">
+            <div class="form-group">
+              <button type="submit" href="#" class="btn btn-warning"> Build XML</button>
+              <?php  
+              $xml = $this->session->flashdata('xml');
+              if($xml)
+              {
+                ?>
+                <a href="#" class="btn btn-success send"><i class="fa fa-save"></i> Send Job</a>
+              <?php } ?>
+            </div>
           </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row">
+       <div id="abortIfStuck" style="display: none;">
+      <div class="col-lg-6 col-md-6 col-xs-12 removeAbort">
+        <div class="box box-primary">
           <div class="box-header with-border">
             <div class="box-tools pull-right">
-              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-              </button>
+              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
             </div>
-            <h3 class="box-title"><b>Options Control Panel</b></h3> 
-          </div>
-          <!-- /.box-header -->
-          <!-- form start -->
-          <div class="box-body">
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"> Trigger Job
-              </label>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"> Build Periodicly
-              </label>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"> Abort the job if it's stuck
-              </label>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" name="timestamp" id="timestamp"> Add timestamps to the Console Output
-              </label>
-            </div>
-            <div class="form-group">
-              <div class="form-group">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Add Job Function
-                    <span class="caret"></span>
-                    <span class="sr-only">Toggle Dropdown</span>
-                  </button>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Execute a Windows Command</a></li>
-                    <li><a href="#">Execute a Linux Command</a></li>
-                  </ul>
+            <h3 class="box-title">
+              <b>Abort the job if its stuck option</b></h3>
+            </div><div class="box-body">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="timeoutStrategy">Timeout Stragegy</label>
+                  <select class="form-control" id="timeoutStrategy" name="timeoutStrategy">
+                    <option value="noActivity">No Activity</option>
+                    <option value="absolute">Absolute</option>
+                  </select>
+                </div>
+              </div>
+               <div class="col-md-6 timeoutSeconds">
+                <div class="form-group">
+                  <label for="timeoutMinutes">Timeout Seconds</label>
+                  <input type="number" class="form-control" id="timeoutSeconds" name="timeoutSeconds" min="60" maxlength="50" autocomplete="off">
+                </div>
+              </div>
+              <div class="col-md-6 timeoutMinutes" style="display: none;">
+                <div class="form-group">
+                  <label for="timeoutMinutes">Timeout Minutes</label>
+                  <input type="number" class="form-control" id="timeoutMinutes" name="timeoutMinutes" min="1"  maxlength="50" autocomplete="off">
                 </div>
               </div>
             </div>
-            <div class="form-group">
-              <div class="form-group">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Actions After Job Execution
-                    <span class="caret"></span>
-                    <span class="sr-only">Toggle Dropdown</span>
-                  </button>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Execute another job</a></li>
-                    <li><a href="#">Email Notification</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Editable Email Notification</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="form-group">
-                <a href="#" class="btn btn-success send"><i class="fa fa-save"></i> Build Job</a>
-              </div>
-            </div>
           </div>
-          <!-- /.box-body -->
-        </form>
+        </div>
+      </div>
+    </form>
+  </div>
+  <div id="output"></div>
+
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-xs-12">
+      <div class="box">
+        <div class="box-header with-border">
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+          </div>
+          <h3 class="box-title"><b>XML Document</b></h3> 
+        </div>
+        <div class="box-body">
+          <div>
+           <?php  
+           $xml = $this->session->flashdata('xml');
+           if($xml)
+           {
+            ?>
+            <div>
+              <pre id="xml" class="xml"><?php echo $this->session->flashdata('xml'); ?> </pre>
+            </div>
+          <?php } ?>
+        </div>
+        <div class="overlay" style="display:none;">
+          <i class="fa fa-refresh fa-spin"></i>
+        </div>
       </div>
     </div>
   </div>
-
-  <div id="output"></div>
+</div>
 
 </section>
 </div>
@@ -142,68 +252,58 @@
 <script type="text/javascript">
   $(document).ready(function(){
 
+    var addUserForm = $("#InsertDbSettings");
+    var validator = addUserForm.validate({
+
+      rules:{
+        job_name :{ required : true },
+        confirmation :{ required : true },
+        description :{ required : true },
+        timeoutMinutes :{ required : true }
+      },
+      messages:{
+        job_name :{ required : "This field is required" },
+        confirmation :{ required : "This field is required" },        
+        description :{ required : "This field is required" }, 
+        timeoutMinutes :{ required : "This field is required" }          
+      }
+    });
+
+
+    $('#abort').click(function(){
+      if($(this).is(":checked")){
+        console.log("Checkbox is checked.");
+
+        $('#abortIfStuck').show();
+        $("#timeoutMinutes").prop('required',true);
+
+        $('#timeoutStrategy').change(function(){
+          var val = $('#timeoutStrategy').val();
+          console.log(val)
+          if (val == 'absolute') {
+            $('.timeoutSeconds').hide();
+            $('.timeoutMinutes').show();
+          } else {
+            $('.timeoutSeconds').show();
+            $('.timeoutMinutes').hide();
+          }
+        });
+     
+      }
+      else if($(this).is(":not(:checked)")){
+        console.log("Checkbox is unchecked.");
+        $('#abortIfStuck').hide();
+      }
+    });
+
 
   // Starts when click on build job button
   $('.send').click(function() {
     // Check if the confirmation checkbox is marked
     if($('#confirmation').is(":checked")){ // confirmation is maked
-     var job_name = $('#job_name').val();
-     var description = $('#description').val();
-
-
-        $.createElement = function(name)
-            {
-              return $('<'+name+' />');
-            };
-
-            $.fn.appendNewElement = function(name)
-            {
-              this.each(function(i)
-              {
-                $(this).append('<'+name+' />');
-              });
-              return this;
-            }
-
-                var $root = $('<XMLDocument />');
-
-                $root.append
-                (
-            // one method of adding a basic structure
-            $('<project />').append( // Create the root node
-              $('<actions/>').append(), // Append Actions Node
-              $('<description />').text(description), // Add Description node
-              $('<keepDependencies />').text("false"), // Add KeepDependencies Node
-              $('<properties/>').append(), // Add Properties Node
-              $('<scm class="hudson.scm.NullSCM"/>').append(), //Add Scm node
-              $('<canRoam />').text("true"), // Add canRoam Node
-              $('<disabled />').text("false"), // Add Disabled Node
-              $('<blockBuildWhenDownstreamBuilding />').text("false"),
-              $('<blockBuildWhenUpstreamBuilding />').text("false"),
-              $('<triggers/>').append(), // Append Triggers Node
-              $('<concurrentBuild />').text("false"),
-              $('<builders/>').append(), // Append Builders Node
-              $('<publishers/>').append(), // Append publishers
-              $('<buildWrappers />').append(
-                $('<hudson.plugins.timestamper.TimestamperBuildWrapper plugin="timestamper@1.10"/>').append()
-              )
-
-
-             )
-            );
-
-              var data = $root.html();
-                console.log(data);
-
-     //Validate fields Message
-     if (job_name == ''){
-      toastr.error("The Job Name field is Null, please fill it", "Error");
-    } else if (description == '') {
-      toastr.error("The Description field is Null, please fill it", "Error");
-    }
-
-    // If Validation is ok
-    if (job_name != '' && description != '') {
+     var job_name = "<?php echo $this->session->flashdata('job_name'); ?>";
+     console.log(job_name);
+     var xml = $('#xml').text();
 
 
         // get Jenkins credentials
@@ -215,17 +315,17 @@
         //Ajax request to post the xml to jenkins api
         $.ajax({
           url: jenkins_url + 'createItem?name='+ job_name ,
-          data: data, 
+          data: xml, 
           method: 'POST',
-         contentType: "text/xml",
+          contentType: "text/xml",
           dataType: "text",
           headers: {'Authorization': 'Basic ' + btoa(jenkins_username + ':' + jenkins_token)},
           beforeSend: function() {
             $('.overlay').show();
 
             $('#input-form').each (function(){
-            this.reset();
-          });
+              this.reset();
+            });
           }
         }).done(function(data) {
           $('.overlay').hide();
@@ -235,59 +335,24 @@
         }).fail(function() {
           $('.overlay').hide();
           console.error(arguments);
-          toastr.error("Your Job Creation Request Has Been Failed, it seems this job already exist", "Request Error")
+          toastr.error("Your Job Creation Request Has Been Failed", "Request Error")
         });
 
-        console.log("Checkbox is checked.");
+        $('.send').remove();
+        $('.xml').remove();
+        $('.destroy').remove();
 
-      }
 
-
-    }
 
     // Confirmation box is not checked
-   else if($('#confirmation').is(":not(:checked)")){ // confirmation is not marked
+  } else if($('#confirmation').is(":not(:checked)")){ // confirmation is not marked
     toastr.error("Checkbox is unchecked.");
   }
 
+  
 });  
 
 
-
-
-
-
-  function generateXML(){
-
-    $.createElement = function(name)
-    {
-      return $('<'+name+' />');
-    };
-
-    $.fn.appendNewElement = function(name)
-    {
-      this.each(function(i)
-      {
-        $(this).append('<'+name+' />');
-      });
-      return this;
-    }
-
-        var $root = $('<XMLDocument />');
-
-        $root.append
-        (
-    // one method of adding a basic structure
-    $('<project />').append(
-      $('<description />').text('Teste Matheus 123456 Teste')
-      )
-    );
-
-        console.log($root.html());
-
-      }
-      generateXML();
-
-    });
-  </script>
+});
+</script>
 
